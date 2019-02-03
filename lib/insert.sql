16703a467d2b062efd635ce3b1ab10f1f173a12b
Marie Burns <mcburns2222@gmail.com>
2019-02-03 20:42:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,5 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "brown", "wild", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "Female")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "Female", "red", "rowdy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "F", "black", "good", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 29, "F", "black", "stern", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 45, "F", "purple", "kind", 1)
